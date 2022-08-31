634b37edc314aa99e1914e6b59d266cd89cd8167
Danil_Rybkin <danil.rybkin.19@gmail.com>
2022-08-31 20:09:39 +0300

    changed showMoreStyles module

--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -1,42 +1,62 @@
-import { getResourse } from "../services/requests";
-import Toast from "./toast";
+// import { getResourse } from "../services/requests";
+// import Toast from "./toast";
 
-const showMoreStyles = (trigger, wrapper) => {
-    const btn = document.querySelector(trigger);
+// const showMoreStyles = (trigger, wrapper) => {
+//     const btn = document.querySelector(trigger);
 
-    btn.addEventListener('click', function () {
-        getResourse('http://localhost:3000/style')
-            .then(res => createCards(res))
-            .catch(error => {
-                console.dir(error);
-                new Toast({
-                    title: "Error",
-                    text: error.message,
-                    theme: "light",
-                    autohide: false
-                });
-            });
+//     btn.addEventListener('click', function () {
+//         getResourse('http://localhost:3000/style')
+//             .then(res => createCards(res))
+//             .catch(error => {
+//                 console.dir(error);
+//                 new Toast({
+//                     title: "Error",
+//                     text: error.message,
+//                     theme: "light",
+//                     autohide: false
+//                 });
+//             });
 
-        this.remove();
+//         this.remove();
+//     });
+
+//     function createCards(response) {
+//         response.forEach(({ src, title, link }) => {
+//             let card = document.createElement('div');
+
+//             card.classList.add('animated', 'fadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+
+//             card.innerHTML = `
+//                 <div class="styles-block">
+//                     <img src=${src} alt="style">
+//                     <h4>${title}</h4>
+//                     <a href=${link}>Подробнее</a>
+//                 </div>  
+//             `;
+
+//             document.querySelector(wrapper).appendChild(card);
+//         });
+//     }
+// };
+
+// export default showMoreStyles;
+
+const showMoreStyles = (trigger, styles) => {
+    const cards = document.querySelectorAll(styles),
+          btn = document.querySelector(trigger);
+
+    cards.forEach(card => {
+        card.classList.add('animated', 'fadeInUp');
     });
 
-    function createCards(response) {
-        response.forEach(({ src, title, link }) => {
-            let card = document.createElement('div');
-
-            card.classList.add('animated', 'fadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
-
-            card.innerHTML = `
-                <div class="styles-block">
-                    <img src=${src} alt="style">
-                    <h4>${title}</h4>
-                    <a href=${link}>Подробнее</a>
-                </div>  
-            `;
-
-            document.querySelector(wrapper).appendChild(card);
+    btn.addEventListener('click', () => {
+        cards.forEach(card => {
+            card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
+            card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
         });
-    }
+        // btn.style.display = 'none';
+        btn.remove();
+    });
 };
 
 export default showMoreStyles;
